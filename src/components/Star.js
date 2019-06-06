1685425b48f3d50b30c221fccef8e02acfdbef62
jeenoo <jw.lim@theenm.com>
2019-06-06 16:31:22 +0900

    중간저장

--- src/components/Star.js
+++ src/components/Star.js
@@ -12,7 +12,7 @@ export default class Star extends Component {
 	onClickHandler = async () => {
 		const { index, amount } = this.props;
 		const { active } = this.state;
-		await this.setState({ active: index >= amount });
+		await this.setState({ active: index >= amount ? !active : active });
 		this.props.onParentClick(index + (this.state.active ? 0 : -1));
 	};
 
