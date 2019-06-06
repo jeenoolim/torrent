cdae77ef492b239a7fc025a0822ec33dc4350b61
jeenoo <jw.lim@theenm.com>
2019-06-06 16:30:57 +0900

    중간저장

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,16 +3,41 @@ import Star from './Star';
 
 export default class Rating extends Component {
 	state = {
-		amount: this.props.amount
+		amount: this.props.amount,
+		mouseDown: false
 	};
 
 	onChildClick = n => {
 		this.setState({ amount: n });
 		this.props.onChange(n);
 	};
+
+	onMouseDown = e => {
+		this.setState({ mouseDown: true });
+	};
+
+	onMouseUp = e => {
+		this.setState({ mouseDown: false });
+	};
+
+	onMouseMove = e => {
+		const { mouseDown } = this.state;
+		if (mouseDown) {
+			const { x, width } = e.currentTarget.getBoundingClientRect();
+			const rating = Math.round((e.clientX - x) / width * 10);
+			this.setState({ amount: rating });
+			this.props.onChange(rating);
+		}
+	};
+
 	render() {
 		return (
-			<div className="rating">
+			<div
+				className="rating"
+				onMouseDown={this.onMouseDown}
+				onMouseUp={this.onMouseUp}
+				onMouseMove={this.onMouseMove}
+			>
 				{[...Array(this.props.max).keys()]
 					.map(n => n + 1)
 					.map(n => (
